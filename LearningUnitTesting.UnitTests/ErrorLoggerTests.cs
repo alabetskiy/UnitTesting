45ab4486f09dc462e0c5ec14e88abd44e2d25234
alabetskiy <labetskiy@gmail.com>
2018-02-18 00:20:58 -0500

    Testing void mathods

---
+++ LearningUnitTesting.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,19 @@
+﻿using LearningUnitTesting.Fundamentals;
+using NUnit.Framework;
+
+namespace LearningUnitTesting.UnitTests
+{
+    [TestFixture]
+    public class ErrorLoggerTests
+    {
+        [Test]
+        public void Log_WhenCalled_SetTheLastErrorProperty()
+        {
+            var logger = new ErrorLogger();
+            logger.Log("test");
+            
+            Assert.That(logger.LastError, Is.EqualTo("test"));
+            
+        }
+    }
+}
