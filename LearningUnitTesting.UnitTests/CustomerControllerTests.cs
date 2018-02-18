5b52132bafbcfd6993a20193d50b2807c0c18e3a
alabetskiy <labetskiy@gmail.com>
2018-02-18 00:10:36 -0500

    Added test when method returns instance

---
+++ LearningUnitTesting.UnitTests/CustomerControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using LearningUnitTesting.Fundamentals;
+using NUnit.Framework;
+
+namespace LearningUnitTesting.UnitTests
+{
+    [TestFixture]
+    public class CustomerControllerTests
+    {
+
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFound()
+        {
+            var controller = new CustomerController();
+
+            var result = controller.GetCustomer(0); 
+            
+            //NotFound object
+            Assert.That(result, Is.TypeOf<NotFound>()); //Use this most of the time. TypeOf<Object> ensure that it is exactly this object. 
+            
+            //NotFound object or one of its derivatives
+            // Assert.That(result, Is.InstanceOf<NotFound>()); //Result can be NotFound object or on of its derivatives
+        }
+        
+        [Test]
+        public void GetCustomer_IdIsNotZero_ReturnsOk()
+        {
+            
+        }
+    
+    }
+}
