5b01cbef8803d898753cdc325ad71439018c2b03
alabetskiy <labetskiy@gmail.com>
2018-02-27 17:31:47 -0500

    Testing methods that returns string

---
+++ LearningUnitTesting.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,45 @@
+﻿using LearningUnitTesting.Fundamentals;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
+using NUnit.Framework;
+
+namespace LearningUnitTesting.UnitTests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [Test]
+        public void GetOutput_DevidedByThreeAndFive_ReturnFizzBuzz()
+        {
+
+            var result = FizzBuzz.GetOutput(15);
+            Assert.That(result, Is.EqualTo("FizzBuzz"));
+        }
+
+        [Test]
+        public void GetOutput_DevidedByThree_ReturnFizz()
+        {
+
+            var result = FizzBuzz.GetOutput(9);
+            Assert.That(result, Is.EqualTo("Fizz"));
+        }
+        
+        [Test]
+        public void GetOutput_DevidedByFive_ReturnBuzz()
+        {
+
+            var result = FizzBuzz.GetOutput(10);
+            Assert.That(result, Is.EqualTo("Buzz"));
+        }
+        
+        [Test]
+        public void GetOutput_DevidedAnyNumber_ReturnTheSameNumber()
+        {
+
+            var result = FizzBuzz.GetOutput(11);
+            Assert.That(result, Is.EqualTo("11"));
+        }
+        
+        
+
+    }
+}
