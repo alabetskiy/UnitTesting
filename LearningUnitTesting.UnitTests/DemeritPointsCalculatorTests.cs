b71e90d0135d7fa8932afa1e2b846d7ac064658c
alabetskiy <labetskiy@gmail.com>
2018-02-28 19:05:43 -0500

    Testing methods that returns int

---
+++ LearningUnitTesting.UnitTests/DemeritPointsCalculatorTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using LearningUnitTesting.Fundamentals;
+using NUnit.Framework;
+
+namespace LearningUnitTesting.UnitTests
+{
+    [TestFixture]
+    public class DemeritPointsCalculatorTests
+    {
+        [Test]
+        [TestCase(-1)]
+        [TestCase(301)]
+        public void CalculateDetermitPoints_SpeedIsOutOfRange_ThrowExeption(int speed)
+        {
+            var calculator = new DemeritPointsCalculator();
+
+            Assert.That(()=>calculator.CalculateDemeritPoints(speed),
+                Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
+            
+        }
+       
+        
+        [Test]
+        [TestCase(0,0)]
+        [TestCase(64,0)]
+        [TestCase(65,0)]
+        [TestCase(66,0)]
+        [TestCase(70,1)]
+        [TestCase(75,2)]
+        public void CalculateDetermitPoints_WhenCalled_ReturnDemeritPoints(int speed, int expectedResult)
+        {
+            var calculator = new DemeritPointsCalculator();
+            var points = calculator.CalculateDemeritPoints(speed);
+            
+            Assert.That(points, Is.EqualTo(expectedResult));
+        }
+
+    }
+}
