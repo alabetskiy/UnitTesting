beeae3309de5bf7db79bbbbb574909c0d092fb3c
alabetskiy <labetskiy@gmail.com>
2018-03-13 16:58:52 -0400

    Working with Moq library to mock external dependencies.

--- LearningUnitTesting.UnitTests/Mocking/VideoServiceTests.cs
+++ LearningUnitTesting.UnitTests/Mocking/VideoServiceTests.cs
@@ -1,4 +1,5 @@
 ﻿using LearningUnitTesting.Mocking;
+using Moq;
 using NUnit.Framework;
 
 namespace LearningUnitTesting.UnitTests.Mocking
@@ -6,14 +7,25 @@ namespace LearningUnitTesting.UnitTests.Mocking
     [TestFixture]
     public class VideoServiceTests
     {
+        private VideoService _videoService;
+        private Mock<IFileReader> _mockFileReader;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockFileReader = new Mock<IFileReader>();
+            _videoService = new VideoService(_mockFileReader.Object);
+
+        }
+        
+
         [Test]
         public void ReadVideoTitle_EmptyFile_ReturnError()
         {
-            var service = new VideoService();
-            
-            service._fileReader = new FakeFileReader();
+                      
+            _mockFileReader.Setup(fr => fr.Read("video.txt")).Returns(""); //settting up mockFileReader object
 
-            var result = service.ReadVideoTitle();
+            var result = _videoService.ReadVideoTitle();
             
             Assert.That(result, Does.Contain("error").IgnoreCase);
         }
