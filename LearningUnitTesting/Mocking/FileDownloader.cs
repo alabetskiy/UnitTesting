2a43109c976af9995385b829fc3ce784a342b44f
alabetskiy <labetskiy@gmail.com>
2018-03-28 22:03:53 -0400

    Refactored InstallerHelper method

---
+++ LearningUnitTesting/Mocking/FileDownloader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace LearningUnitTesting.Mocking
+{
+    public class FileDownloader : IFileDownloader
+    {
+        public void DownloadFile(string url, string path)
+        {
+            var client = new WebClient();
+
+            client.DownloadFile(url, path);
+
+        }
+    }
+
+    public interface IFileDownloader
+    {
+        void DownloadFile(string url, string path);
+    }
+}
